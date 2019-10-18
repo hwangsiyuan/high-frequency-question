5083c9b43927b349faecaad2cfb0cddf06a2befd
hwangsy <siyuan.huang@fenbeitong.com>
2019-10-18 17:32:17 +0800

    tp

--- jdk-question/src/main/java/com/hussein/ThreadPoolTest.java
+++ jdk-question/src/main/java/com/hussein/ThreadPoolTest.java
@@ -16,11 +16,18 @@ public class ThreadPoolTest {
     /**
      * 降低系统消耗，方便管理线程，提高响应速度
      * 线程池 参数列表 核心线程数，最大线程数，超过core的线程空闲保持时间，线程工厂，拒绝策略
+     * 当调用 execute() 方法添加一个任务时，线程池会做如下判断：
+     * a. 如果正在运行的线程数量小于 corePoolSize，那么马上创建线程运行这个任务；
+     * b. 如果正在运行的线程数量大于或等于 corePoolSize，那么将这个任务放入队列。
+     * c. 如果这时候队列满了，而且正在运行的线程数量小于 maximumPoolSize，那么还是要创建线程运行这个任务；
+     * d. 如果队列满了，而且正在运行的线程数量大于或等于 maximumPoolSize，那么线程池会抛出异常，告诉调用者“我不能再接受任务了”。
+     * e.当一个线程完成任务时，它会从队列中取下一个任务来执行。
+     * f.当一个线程无事可做，超过一定的时间（keepAliveTime）时，线程池会判断，如果当前运行的线程数大于 corePoolSize，那么这个线程就被停掉。所以线程池的所有任务完成后，它最终会收缩到 corePoolSize 的大小。
      */
     public static void main(String[] args) {
         BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(10);
         ThreadFactory threadFactory = Executors.defaultThreadFactory();
-        RejectedExecutionHandler rejectedExecutionHandler = new MyAbortPolicy();
+        RejectedExecutionHandler rejectedExecutionHandler = new ThreadPoolExecutor.AbortPolicy();
         ThreadPoolExecutor executor = new ThreadPoolExecutor(2, 4, 60, TimeUnit.SECONDS, queue, threadFactory, rejectedExecutionHandler);
         IntStream.range(1, 100).forEach(i -> {
             executor.execute(() -> {
@@ -32,13 +39,5 @@ public class ThreadPoolTest {
                 System.out.println(Thread.currentThread().getName() + ":" + i);
             });
         });
-    }
-
-    private static class MyAbortPolicy extends ThreadPoolExecutor.AbortPolicy {
-
-        @Override
-        public void rejectedExecution(Runnable r, ThreadPoolExecutor e) {
-            super.rejectedExecution(r, e);
-        }
     }
 }
