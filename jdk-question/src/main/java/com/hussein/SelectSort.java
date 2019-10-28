1bf228a3238c2ed9e2e3de2d476dcd4b6e61e4fe
hwangsy <siyuan.huang@fenbeitong.com>
2019-10-28 19:51:48 +0800

    select sort

---
+++ jdk-question/src/main/java/com/hussein/SelectSort.java
@@ -0,0 +1,48 @@
+package com.hussein;
+
+/**
+ * <p>Title: SelectSort</p>
+ * <p>Description: </p>
+ * <p>Company: www.hussein.com</p>
+ *
+ * @author hwangsy
+ * @date 2019/10/28 7:45 PM
+ */
+public class SelectSort {
+
+    /**
+     * 选择排序
+     *
+     * @param arr 数组
+     * @param n   数组长度
+     */
+    public static void selectSort(int[] arr, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            if (i != min) {
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {20, 40, 30, 10, 60, 50};
+        printArray(a);
+        selectSort(a, a.length);
+        printArray(a);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%d ", arr[i]);
+        }
+        System.out.print("\n");
+    }
+}
